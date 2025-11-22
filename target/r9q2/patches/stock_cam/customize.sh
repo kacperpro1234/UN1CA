17d324b8a56be25d546684f98fa1b1ca9b80f369
kacperpro1234 <kacperdysz849@gmail.com>
2025-11-22 14:37:03 +0100

    Update customize.sh

--- target/r9q2/patches/stock_cam/customize.sh
+++ target/r9q2/patches/stock_cam/customize.sh
@@ -1,3 +1,7 @@
+# Usuwanie plików optymalizacji (to jest bezpieczne i czyści śmieci)
 DELETE_FROM_WORK_DIR "system" "system/priv-app/SamsungCamera/oat"
 DELETE_FROM_WORK_DIR "system" "system/priv-app/SamsungCamera/SamsungCamera.apk.prof"
-ADD_TO_WORK_DIR "r9qxxx" "system" "system/priv-app/SamsungCamera/SamsungCamera.apk" 0 0 644 "u:object_r:system_file:s0"
+
+# Poniższa linia została zakomentowana, ponieważ folder "r9qxxx" nie istnieje.
+# Odkomentuj ją tylko wtedy, gdy dodasz własny plik SamsungCamera.apk do folderu patches/stock_cam/
+# ADD_TO_WORK_DIR "r9qxxx" "system" "system/priv-app/SamsungCamera/SamsungCamera.apk" 0 0 644 "u:object_r:system_file:s0"
