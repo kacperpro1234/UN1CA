f02b3765b36d93fe89898bda48da69b2aae53816
kacperpro1234 <kacperdysz849@gmail.com>
2025-11-22 13:51:37 +0100

    Update customize.sh

--- target/r9q2/patches/stock/customize.sh
+++ target/r9q2/patches/stock/customize.sh
@@ -1,4 +1,3 @@
-
 echo "- Adding stock system features"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/permissions/com.sec.feature.cover.clearsideviewcover.xml" 0 0 644 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/permissions/com.sec.feature.pocketmode_level33.xml" 0 0 644 "u:object_r:system_file:s0"
@@ -8,9 +7,9 @@ echo "- Adding stock GameDriver"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/GameDriver-SM8350/GameDriver-SM8350.apk" 0 0 644 "u:object_r:system_file:s0"
 
 
-
-echo "- Adding stock btservices apex"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/apex/com.android.btservices.apex"
+# Poniższe dwie linijki zostały zakomentowane, aby naprawić błąd "File not found"
+# echo "- Adding stock btservices apex"
+# ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/apex/com.android.btservices.apex"
 
 
 echo "- Fixing artifacting and WiFi"
