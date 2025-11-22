f65f4d23c02eaa029ffdbe9ff58216629592589a
kacperpro1234 <kacperdysz849@gmail.com>
2025-11-22 14:34:38 +0100

    Update customize.sh

--- target/r9q2/patches/camera/customize.sh
+++ target/r9q2/patches/camera/customize.sh
@@ -1,8 +1,13 @@
+# Usuń lub zakomentuj linię powodującą błąd (camera-feature.xml)
+# ADD_TO_WORK_DIR "$DEVICE_DIR/camera/camera-feature.xml" "system" "system/cameradata/camera-feature.xml"
+
+# Dodawanie bibliotek Samsunga
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libSwIsp_core.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libSwIsp_wrapper_v1.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libdualcam_portraitlighting_gallery_360.so" 0 0 644 "u:object_r:system_lib_file:s0"
+
+# Usuwanie konfliktującej biblioteki
 DELETE_FROM_WORK_DIR "system" "system/lib64/libdualcam_refocus_image.so"
 
-
-# Fix Portrait
+# Fix Portrait (Naprawa trybu portretowego)
 sed -i "s/ro.product.name/ro.unica.camera/g" "$WORK_DIR/system/system/lib64/libDualCamBokehCapture.camera.samsung.so"
