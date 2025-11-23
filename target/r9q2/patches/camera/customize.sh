ff5af47d2a2910b116f77f8c212cb2074a5df4a6
kacperpro1234 <kacperdysz849@gmail.com>
2025-11-23 14:00:24 +0100

    Update customize.sh

--- target/r9q2/patches/camera/customize.sh
+++ target/r9q2/patches/camera/customize.sh
@@ -1,13 +1,22 @@
-# Usuń lub zakomentuj linię powodującą błąd (camera-feature.xml)
-# ADD_TO_WORK_DIR "$DEVICE_DIR/camera/camera-feature.xml" "system" "system/cameradata/camera-feature.xml"
+# ==========================================
+# S21 FE (r9q) - ImageTagger & Polarr Blobs
+# ==========================================
 
-# Dodawanie bibliotek Samsunga
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libSwIsp_core.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libSwIsp_wrapper_v1.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libdualcam_portraitlighting_gallery_360.so" 0 0 644 "u:object_r:system_lib_file:s0"
+# Add ImageTagger blobs (Rozpoznawanie obiektów/scen)
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libImageTagger.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
-# Usuwanie konfliktującej biblioteki
-DELETE_FROM_WORK_DIR "system" "system/lib64/libdualcam_refocus_image.so"
+# Add Polarr blobs (Silnik edycji/remasteringu)
+# Konfiguracja bibliotek publicznych
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/public.libraries-polarr.txt" 0 0 644 "u:object_r:system_file:s0"
 
-# Fix Portrait (Naprawa trybu portretowego)
-sed -i "s/ro.product.name/ro.unica.camera/g" "$WORK_DIR/system/system/lib64/libDualCamBokehCapture.camera.samsung.so"
+# Biblioteki dynamiczne Polarr
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libBestComposition.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libFeature.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libPolarrSnap.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libTracking.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libYuv.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+
+# Opcjonalnie: Często wymagane dla funkcji gumki (Object Eraser), jeśli nie są obecne
+# Odkomentuj poniższe linie, jeśli funkcje usuwania obiektów nie działają:
+# ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libInpainting.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+# ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libObjectRemoval.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
